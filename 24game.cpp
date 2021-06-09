c09225f6f86229f1f250ad4a69dc20983bd2f3b2
DidoFayed <80528848+DidoFayed@users.noreply.github.com>
2021-06-09 22:26:56 +0700

    update commit

--- 24game.cpp
+++ 24game.cpp
@@ -48,55 +48,44 @@ bool has_unbound_operator(string msg, string op)
 	return res;
 }
 
+string update_message(string lhs, string rhs, char op)
+{
+	if (op == '+')
+		return lhs + ' ' + op + ' ' + rhs;
+	else if (op == '-')
+	{
+		// if the right operand contains an unbounded minus sign
+		if (has_unbound_operator(rhs, "-"))
+			rhs = '(' + rhs + ')';
+		return lhs + ' ' + op + ' ' + rhs;
+	}
+	else if (op == '*')
+	{
+		if (has_unbound_operator(lhs, "+-"))
+			lhs = '(' + lhs + ')';
+		// need to add paranthensis if there is unbound plus or minus sign
+		if (has_unbound_operator(rhs, "+-"))
+			rhs = '(' + rhs + ')';
+		// need to add paranthesis if there is unbound plus or minus sign
+		return lhs + ' ' + op + ' ' + rhs;
+	}
+
+	else if (op == '/')
+	{
+		if (has_unbound_operator(lhs, "+-"))
+			lhs = '(' + lhs + ')';
+		if (has_unbound_operator(rhs, "+-/"))
+			rhs = '(' + rhs + ')';
+		return lhs + ' ' + op + ' ' + rhs;
+	}
+	else
+		cerr << "There is invalid operation" << endl;
+}
+
 bool compareDouble(double x, double y)
 {
 	int diff = x - y;
 	return diff < 1e-5 && -diff < 1e-5;
-}
-
-bool Solve24_rec(vector<card>& nums, string& solution)
-{
-	int n = nums.size();
-	if (n == 0)
-		return false;
-	else if (n == 1 && compareDouble(nums[0].val, 24.0))
-	{
-		solution = nums[0].msg; // record how the answer is obtained
-		return true;
-	}
-	else if (n == 1)
-		return false;
-
-	for (int i = 0; i < n; ++i)
-		for (int j = i + 1; j < n; ++j)
-		{
-			card left = nums[i];
-			card right = nums[j];
-
-			vector<card> nums_rest;
-			for (int k = 0; k < n; ++k)
-				if (k != i && k != j)
-					nums_rest.push_back(nums[k]);
-
-			for (int k = 0; k < 6; ++k)
-			{
-				// try all possible six operations with the two numbers
-				// notice that left - right and right - left, left / right and right / left are different operations
-				vector<card> temp = nums_rest;
-				if (k == 0) temp.push_back({ left.val + right.val, update_message(left.msg, right.msg, '+') });
-				else if (k == 1) temp.push_back({ left.val - right.val, update_message(left.msg, right.msg, '-') });
-				else if (k == 2) temp.push_back({ right.val - left.val, update_message(right.msg, left.msg, '-') });
-				else if (k == 3) temp.push_back({ left.val * right.val, update_message(left.msg, right.msg, '*') });
-				else if (k == 4 && right.val != 0) temp.push_back({ left.val / right.val, update_message(left.msg, right.msg, '/') });
-				else if (k == 5 && left.val != 0) temp.push_back({ right.val / left.val, update_message(left.msg, right.msg, '/') });
-
-				if (Solve24_rec(temp, solution))
-					return true;
-			}
-
-		}
-
-	return false;
 }
 
 int main()
